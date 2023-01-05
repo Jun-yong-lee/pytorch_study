6e8b0725532a7a19d4e84e4e8645c93d7082d74a
Jun-yong-lee <wnsdyd1569@naver.com>
2023-01-05 18:42:43 +0900

    function make_tensor

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import torch
 
-print(torch.__version__)
+# print(torch.__version__)
+
+def make_tensor():
+    # int16
+    a = torch.tensor([[1, 2], [3, 4]], dtype=torch.int16)
+    # float
+    b = torch.tensor([2], dtype=torch.float32)
+    # double
+    c = torch.tensor([3], dtype=torch.float64)
+    
+    print(a, b, c)
+    
+    tensor_list = [a, b, c]
+    
+    for t in tensor_list:
+        print(f"shape of tensor {t.shape}")
+        print(f"datatype of tensor {t.dtype}")
+        print(f"device tensor is stored on {t.device}")
+    
+if __name__ == "__main__":
+    make_tensor()
