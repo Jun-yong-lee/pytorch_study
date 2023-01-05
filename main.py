45552accd14588fb9eacd2a6a8e47d3df40b8c72
Jun-yong-lee <wnsdyd1569@naver.com>
2023-01-05 19:04:35 +0900

    function reshape_tensor()

--- main.py
+++ main.py
@@ -35,8 +35,33 @@ def sumsub_tensor():
     sum_element_a = a.sum()
     print(f"sum_element_a : {sum_element_a}")
     
-
-
+def muldiv_tensor():
+    a = torch.arange(0, 9).view(3, 3)
+    b = torch.arange(0, 9).view(3, 3)
+    print(f"input tensor :\n {a} \n {b}")
+    
+    # mat_mul
+    c = torch.matmul(a, b) # matrix multiplication
+    print(f"mat_mul : {c}")
+    
+    # elementwise multiplication
+    d = torch.mul(a, b)
+    print(f"elementwise mul : {d}")
+    
+def reshape_tensor():
+    a = torch.tensor([2, 4, 5, 6, 7, 8])
+    print(f"input tensor : \n {a}")
+    
+    # view
+    b = a.view(2, 3)
+    print(f"view \n {b}")
+    
+    # transpose
+    bt = b.t()
+    print(f"transpose \n {bt}")
+    
 if __name__ == "__main__":
     # make_tensor()
-    sumsub_tensor()
+    # sumsub_tensor()
+    # muldiv_tensor()
+    reshape_tensor()
