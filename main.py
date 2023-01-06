1f42f919bbac0dd011ef8dfe2605062c0e9f86fe
Jun-yong-lee <wnsdyd1569@naver.com>
2023-01-07 00:51:30 +0900

    code convolution operation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import numpy as np
+import time
+import torch
+import torch.nn as nn
 
 from function.convolution import Conv
 
@@ -7,15 +10,15 @@ def convolution():
     
     # define the shape of input & weight
     
-    in_w = 3
-    in_h = 3
-    in_c = 1
+    in_w = 6
+    in_h = 6
+    in_c = 3
     out_c = 16
     batch = 1
     k_w = 3
     k_h = 3
     
-    X = np.arange(9, dtype=np.float32).reshape([batch, in_c, in_h, in_w])
+    X = np.arange(in_w * in_h * in_c * batch, dtype=np.float32).reshape([batch, in_c, in_h, in_w])
     W = np.array(np.random.standard_normal([out_c, in_c, k_h, k_w]), dtype=np.float32)
     
     Convolution = Conv(batch=batch,
@@ -29,11 +32,18 @@ def convolution():
                 stride=1,
                 pad=0)
     
-    print(f"X = {X}")
-    print(f"W = {W}, W.shape = {W.shape}")
-    L1 = Convolution.conv(X, W)
-    print(f"L1 shape : {L1.shape}")
-    print(f"L1 : {L1}")
+    # print(f"X = {X}")
+    # print(f"W = {W}, W.shape = {W.shape}")
     
+    L1_time = time.time
+    
+    for i in range(5):
+        L1 = Convolution.conv(X, W)
+    print(f"L1 time : {time.time() - L1_time}")
+
+    for i in range(5):
+        L2 = Convolution.gemm(X, W)
+    print(f"L2 time : {time.time() - L2_time}")
+
 if __name__ == "__main__":
     convolution()
