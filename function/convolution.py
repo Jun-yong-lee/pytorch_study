e2e14c3fad9553b42b0131f6f01cf8e850598786
Jun-yong-lee <wnsdyd1569@naver.com>
2023-01-06 23:44:16 +0900

    code class Conv

--- function/convolution.py
+++ function/convolution.py
@@ -13,5 +13,34 @@ class Conv:
         self.stride = stride
         self.pad = pad
         
-    # naive convolution Sliding window metric
+        self.out_h = (in_h - k_h + 2 * pad) // stride + 1
+        self.out_w = (in_w - k_w + 2 * pad) // stride + 1
+    
+    def check_range(self, a, b):
+        return a > -1 and a < b
         
+    # naive convolution Sliding window metric
+    def conv(self, A, B):
+        C = np.zeros((self.batch, self.out_c, self.out_h, self.out_w), dtype=np.float32)
+        
+        # seven loop
+        for b in range(self.batch):
+            for oc in range(self.out_c):
+                # each channel of output
+                for oh in range(self.out_h):
+                    for ow in range(self.out_w):
+                        # one pixel of output shape
+                        a_j = oh * self.stride - self.pad
+                        for kh in range(self.k_h):
+                            if self.check_out(a_j, self.in_h) == False:
+                                C[b, oc, oh, ow] += 0
+                            else:
+                                a_i = ow * self.stride - self.pad
+                            for kw in range(self.k_w):
+                                if self.check_range(a_i, self.in_w) == False:
+                                    C[b, oc, oh, ow] += 0
+                                else:
+                                    C[b, oc, oh, ow] += np.dot(A[b, :, a_j, a_i], B[oc, :, kh, kw])
+                                a_i += self.stride
+                            a_j += self.stride
+        return C
