710ca2973cbcc135685efc68016df85bfe695dcb
Jun-yong-lee <wnsdyd1569@naver.com>
2023-01-06 11:35:05 +0900

    code lenet5.py

---
+++ model/lenet5.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+
+class Lenet5(nn.Module):
+    def __init__(self, batch, n_classes, in_channel, in_width, in_height, is_train=False):
+        super().__init__()
+        self.batch = batch
+        self.n_classes = n_classes
+        self.in_width = in_width
+        self.in_height = in_height
+        self.in_channel = in_channel
+        self.is_train = is_train
+        
+        # convolution output : [(W - K + 2P)/S] + 1
+        
+        # [(32 - 5 + 2*0) / 1] + 1 = 28
+        self.conv0 = nn.Con2d(self.in_channel, 6, kernel_size=5, stride=1, padding=0)
+        # self.pool1 =
+        
+        self.conv1 = nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0)
+        
+        self.conv2 = nn.Conv2d(16, 120, kernel_size=5, stride=1, padding=0)
+        
+        # fully-connected layer
+        self.fc3 = nn.Linear(120, 84)
+        self.fc4 = nn.Linear(84, self.n_classes)
+        
+    def forward(self, x):
+        # x' shape : [B, C, H, W]
+        x = self.conv0(x)
+        x = torch.tanh(x)
+        x = nn.functional.avg_pool2d(x, kernel_size=2, stride=2)
+        x = self.conv1(x)
+        x = torch.tanh(x)
+        x = nn.functional.avg_pool2d(x, kernel_size=2, stride=2)
+        x = self.conv2(x)
+        x = torch.tanh(x)
+        # change format from 4dim -> 2dim ( [B, C, H, W] -> [B, C*H*W] )
+        x = torch.flatten(x, start_dim=1)
+        x = self.fc3(x)
+        x = torch.tanh(x)
+        x = self.fc4(x)
+        x = x.view(self.batch, -1)
+        x = nn.functional.softmax(x, dim=1)
+        
+        if self.is_train is False:
+            x = torch.argmax(x, dim=1)
+        return x
+        
