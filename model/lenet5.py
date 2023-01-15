c103c540d585bae1e273adbe5080cc84d8e6cf25
Jun-yong-lee <wnsdyd1569@naver.com>
2023-01-15 19:03:06 +0900

    test

--- model/lenet5.py
+++ model/lenet5.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 
 class Lenet5(nn.Module):
-    
     def __init__(self, batch, n_classes, in_channel, in_width, in_height, is_train=False):
         super().__init__()
         self.batch = batch
